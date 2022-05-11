c731de68aa50b450d0b436b2e44b814a6a0daf89
i-atanasov <ivayloatanasov@zohomail.eu>
2022-05-11 17:47:01 +0300

    more preps

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from "react";
+import Accordion from "./components/Accordion";
 
-class App extends React.Component {
-    
-    render() {
-        return <div>App</div>;
-    };
-}
+const items = [
+    {
+        title: 'What is React?',
+        content: 'Front end js framework.'
+    },
+    {
+        title: 'Why use it?',
+        content: 'It is a favorite among engineers.'
+    },
+    {
+        title: 'How to use it?',
+        content: 'By creating components'
+    }
+];
+
+const App = () => {
+    return (
+        <div>
+            <h2>App</h2>
+            <Accordion items={items}/>
+        </div>
+    );
+};
 
 export default App;
