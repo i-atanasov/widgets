07edc102ee6734c8309d964e27bcce3344f35838
i-atanasov <ivayloatanasov@zohomail.eu>
2022-05-12 11:36:26 +0300

    final accordion

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import React from "react";
 import Accordion from "./components/Accordion";
-
-const items = [
-    {
-        title: 'What is React?',
-        content: 'Front end js framework.'
-    },
-    {
-        title: 'Why use it?',
-        content: 'It is a favorite among engineers.'
-    },
-    {
-        title: 'How to use it?',
-        content: 'By creating components'
-    }
-];
+import { items } from "./data";
 
 const App = () => {
     return (
         <div>
-            <h2>App</h2>
+            <h4 className="ui large header">National records:</h4>
             <Accordion items={items}/>
         </div>
     );
---
+++ src/data.js
@@ -0,0 +1,19 @@
+export const items = [
+    {
+        title: '5000 m',
+        content: '13:13.15	Evgeni Ignatov:	Stutgart, 31 08 1986'
+
+    },
+    {
+        title: '10000 m',
+        content: '27:56.26  Evgeni Ignatov: Oslo, 02 07 1988'
+    },
+    {
+        title: 'Half Marathon',
+        content: '63:22 Petko Stefanov: Podgoritsa, 27 10 2002'
+    },
+    {
+        title: 'Marathon',
+        content: '2:11:26 Hristo Stefanov: Sofia, 19 10 1997'
+    }
+];
